2de60b8ec5a5ce504b3841307f0174cb0a2bbff5
Mark Anthony Navarro <navarro.markanthony.tud@gmail.com>
2024-12-17 22:58:49 +0800

    updated text-to-voice-app 🎙️

--- text-to-audio-app/script.js
+++ text-to-audio-app/script.js
@@ -2,18 +2,21 @@ function textToAudio() {
     const text = document.querySelector('.text').value;
 
     if (text.trim() === '') {
-        alert('Please enter some text to convert to audio!');
+        document.getElementById('warningModal').style.display = 'flex';
         return;
     }
 
     window.speechSynthesis.cancel();
 
     const speech = new SpeechSynthesisUtterance();
-
     speech.text = text;
     speech.rate = 1;
-    speech.pitch = 1; 
+    speech.pitch = 1;
     speech.volume = 1;
 
     window.speechSynthesis.speak(speech);
+}
+
+function closeModal() {
+    document.getElementById('warningModal').style.display = 'none';
 }
